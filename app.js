f2a7c512b4ed8741728d43edc6ed22f570205200
v-s-ai <victoria.f.s140@gmail.com>
2024-09-10 13:48:41 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const images = document.querySelectorAll("img");
+    for(const image of images ){
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => image.src = data.message)
+        image.width = 1000
+        image.height =1000
+
+    }
+    
+})
